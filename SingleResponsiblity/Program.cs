5844a674f5a793f1df21ac161bd476e69ded1f76
Nikola Vacic <nikolavacic23@gmail.com>
2021-01-28 12:45:31 +0100

    Initial Commit

---
+++ SingleResponsiblity/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// SingleResposiblity je princip koji kaze da jedna klasa treba da radi samo jednu stvar
+
+namespace SingleResponsiblity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
